339612f22627d021ac6a35eb0061a0a759add0e1
niksync1 <40718182+niksync1@users.noreply.github.com>
2023-07-15 00:56:15 +0000

    added user login and logout

--- src/features/users/UsersList.js
+++ src/features/users/UsersList.js
@@ -9,15 +9,20 @@ const UsersList = () => {
         isSuccess,
         isError,
         error
-    } = useGetUsersQuery()
+    } = useGetUsersQuery(undefined, {
+        pollingInterval: 60000,
+        refetchOnFocus: true,
+        refetchOnMountOrArgChange: true
+    })
 
     let content
 
     if (isLoading) content = <p>Loading...</p>
 
     if (isError) {
-         content = <p className={isError ? "errmsg" : "offscreen"}>{error?.data?.message} </p>
+        content = <p className="errmsg">{error?.data?.message}</p>
     }
+
     if (isSuccess) {
 
         const { ids } = users
